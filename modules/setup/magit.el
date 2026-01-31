0e368c1dd84ca97f0e3ddf84b69075c8cc26329e
gustsavrisagerus@gmail.com <gustavrisagerus@gmail.com>
2026-01-31 12:00:57 +0100

    Added hook to add project to projects when cloned.

--- modules/setup/magit.el
+++ modules/setup/magit.el
@@ -1,6 +1,13 @@
 (use-package magit
   :straight t
   :config
-  (setq magit-clone-default-directory "~/projects/"))
+  (setq magit-clone-default-directory "~/projects/")
+  (defun my/register-cloned-project ()
+    "Register a newly cloned directry as a project"
+    (let ((repo-dir default-directory))
+      (when-let ((pr (project-current nil repo-dir)))
+	(project-remember-project pr)
+	(message "Registered new project: %s" repo-dir))))
+  (add-hook 'magit-post-clone-hook #'my/register-cloned-project))
 
 (provide 'setup/magit)
