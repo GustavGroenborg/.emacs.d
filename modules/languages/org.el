2848bd829109cfe66ee8faab432d5b3e1e20732d
Gustav C. R. Grønborg <gustavrisagerus@gmail.com>
2026-03-12 14:46:48 +0100

    Created function to automatically change state of task

--- modules/languages/org.el
+++ modules/languages/org.el
@@ -1,4 +1,3 @@
-
 (use-package org
   :straight t
   :defer t
@@ -33,13 +32,34 @@
     (forward-line -1)))
   (define-key org-mode-map (kbd "C-c s") 'my/org-insert-python-block)
 
-  ;; Bind it to a fast shortcut (like C-c s) in Org-mode
-  (define-key org-mode-map (kbd "C-c s") 'my/org-insert-auto-session-block)
+  ;; The below two functions is loosly inspired by this answer https://emacs.stackexchange.com/a/22147
+  (defun my/org-child-checkboxes-complete-p ()
+    "Return t if all child checkboxes are marked as complete"
+    (let ((end (line-end-position))
+	  (is-complete nil))
+      (save-excursion
+	(when (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]" end t)
+	  (setq is-complete (if (match-end 1) (string-equal (match-string 1) "100%")
+			      (and (match-string 2) (string-equal (match-string 2) (match-string 3)))))))
+    is-complete))
 
-  ;; Stop Emacs from asking "Do you want to run this?" every time
-  (setq org-confirm-babel-evaluate nil)
+  (defun my/org-update-parent-task-state ()
+    "Updates the state of the parent task when all child checkboxes are ticked."
+    (let ((todo-state (org-get-todo-state)))
+      (when todo-state
+	(save-excursion
+	  (org-back-to-heading t)
+	  (if (my/org-child-checkboxes-complete-p)
+	      (unless (string-equal todo-state "DONE")
+		(org-todo 'done))
+	    ;; Relevant in the case, when checkboxes are unchecked
+	    (unless (string-equal todo-state "TODO")
+	      (org-todo 'todo)))))))
   
   ;; Automatically show plots/images after a block finishes
-  (add-hook 'org-babel-after-execute-hook 'org-display-inline-images))
+  (add-hook 'org-babel-after-execute-hook 'org-display-inline-images)
+
+  ;; Automically updates parent task upon checkbox toggle
+  (add-hook 'org-checkbox-statistics-hook 'my/org-update-parent-task-state))
 
 (provide 'languages/org)
