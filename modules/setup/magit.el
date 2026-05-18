d18de31c5cef4fa1e0ed74bc3e2d88188b68603e
Gustav C. R. Grønborg <gustavrisagerus@gmail.com>
2026-05-18 10:59:25 +0200

    QOL updates to magit setup

--- modules/setup/magit.el
+++ modules/setup/magit.el
@@ -8,6 +8,13 @@
       (when-let ((pr (project-current nil repo-dir)))
 	(project-remember-project pr)
 	(message "Registered new project: %s" repo-dir))))
-  (add-hook 'magit-post-clone-hook #'my/register-cloned-project))
+  (add-hook 'magit-post-clone-hook #'my/register-cloned-project)
+
+  ;; Automatically reload files on, e.g. git checkout
+  (global-auto-revert-mode 1)
+  ;; As above, but for non-file buffer, e.g. dired
+  (setq global-auto-revert-non-file-buffers t)
+  (setq auto-revert-check-vc-info t)
+  (add-hook 'magit-post-checkout-hook (lambda () (force-mode-line-update t))))
 
 (provide 'setup/magit)
